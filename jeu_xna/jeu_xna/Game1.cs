1aebfcaa4a71327a1cad8f87181f90e102d06b3f
thibault <thibault.lafitte@epita.fr>
2014-03-09 16:28:05 +0100

    ceci est un test de github

--- jeu_xna/jeu_xna/Game1.cs
+++ jeu_xna/jeu_xna/Game1.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-namespace jeu_xna
+namespace jeu_xna //CECI EST UN TEST DE GITHUB
 {
     /// <summary>
     /// This is the main type for your game
