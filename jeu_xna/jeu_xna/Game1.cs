cbbbc672e4f8478ec340bd6ba2aa4a2ee39c42eb
thibault <thibault.lafitte@epita.fr>
2014-03-09 16:48:11 +0100

    je ne sais pas

--- jeu_xna/jeu_xna/Game1.cs
+++ jeu_xna/jeu_xna/Game1.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-namespace jeu_xna //CECI EST UN TEST DE GITHUB
+namespace jeu_xna
 {
     /// <summary>
     /// This is the main type for your game
